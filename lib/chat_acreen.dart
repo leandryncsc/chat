54d354d2432f8641589789944f7074711c903aa2
Leandro Bezerra <leandrinhodoema@gmail.com>
2021-09-16 11:59:59 -0300

    alterou nada

--- lib/chat_acreen.dart
+++ lib/chat_acreen.dart
@@ -1,33 +1,30 @@
 import 'dart:io';
-
 import 'package:chat/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
-   @override
+  @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
-  void _sendMessege({String? text, File? imgFile}) async{
-
+  void _sendMessege({String? text, File? imgFile}) async {
     Map<String, dynamic> data = {};
 
-    if(imgFile != null){
-      StorageUploadTask task = FirebaseStorage.instance.ref().child(
-        DateTime.now().microsecondsSinceEpoch.toString()
-      ).putFile(imgFile);
+    if (imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().microsecondsSinceEpoch.toString())
+          .putFile(imgFile);
 
       StorageTaskSnapshot taskSnapshot = await task.onComplete;
       String url = await taskSnapshot.ref.getDownloadURL();
       data['imgUrl'] = url;
     }
 
-    if(text != null) data['text'] = text;
-
+    if (text != null) data['text'] = text;
 
     Firestore.instance.collection('messeges').add(data);
   }
@@ -44,8 +41,8 @@ class _ChatScreenState extends State<ChatScreen> {
           Expanded(
             child: StreamBuilder<QuerySnapshot>(
               stream: Firestore.instance.collection('messenges').snapshots(),
-              builder: (context, snapshot){
-                switch(snapshot.connectionState){
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
                   case ConnectionState.none:
                   case ConnectionState.waiting:
                     return Center(
@@ -55,14 +52,13 @@ class _ChatScreenState extends State<ChatScreen> {
                     List<DocumentSnapshot> documents =
                         snapshot.data!.documents.reversed.toList();
                     return ListView.builder(
-                      itemCount: documents.length,
-                      reverse: true,
-                      itemBuilder:(context, index){
-                        return ListTile(
-                          title: Text(documents[index].data['text']),
-                        );
-                      }
-                    );
+                        itemCount: documents.length,
+                        reverse: true,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(documents[index].data['text']),
+                          );
+                        });
                 }
               },
             ),
